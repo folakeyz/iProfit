03e3dd27ca4189f09ad15e289bebe81a09806ed0
iCode <gbengaobafemi1@gmail.com>
2021-12-27 16:00:15 +0100

    bug fix

--- server.js
+++ server.js
@@ -73,6 +73,19 @@ app.use("/api/v1/instructor", instructor);
 
 app.use(errorHandler);
 
+app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
+
+// if (process.env.NODE_ENV === "production") {
+app.use(express.static(path.join(__dirname, "/public")));
+
+app.get("/*", function (req, res) {
+  res.sendFile(path.join(__dirname, "/public/index.html"), function (err) {
+    if (err) {
+      res.status(500).send(err);
+    }
+  });
+});
+
 const PORT = process.env.PORT || 8000;
 const server = app.listen(
   PORT,
